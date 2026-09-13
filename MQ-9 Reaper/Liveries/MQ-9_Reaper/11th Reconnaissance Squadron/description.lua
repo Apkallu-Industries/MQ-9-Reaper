065dbe1495cf077d15d3610511a988464286f8e1
Dany Mitchel <217238818+Apkallu-Industries@users.noreply.github.com>
2026-09-13 15:40:20 +0100

    feat(liveries): add 432d Wing Hunters, 17th ATKS, and 11th RS squadron liveries

---
+++ MQ-9 Reaper/Liveries/MQ-9_Reaper/11th Reconnaissance Squadron/description.lua
@@ -0,0 +1,14 @@
+livery = {
+    {"Mq9_Reaper_diffuse", 0, "MQ_9_Reaper_diffuse", true},
+    {"Mq9_bort_number_100", 0, "MQ_9_Reaper_diffuse", true},
+    {"Mq9_bort_number_10", 0, "MQ_9_Reaper_diffuse", true},
+    {"Mq9_bort_number_1", 0, "MQ_9_Reaper_diffuse", true},
+}
+
+name = "11th Reconnaissance Squadron (Creech AFB)"
+countries = {
+    "USA",
+    "UK",
+    "France",
+    "Italy"
+}
